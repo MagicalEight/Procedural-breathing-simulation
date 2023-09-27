a982ab9949c4d65a3d652a7c53711ba18ad31223
MagicalEight <108913383+MagicalEight@users.noreply.github.com>
2023-09-27 17:48:09 +0800

    <misc> 调整字段名

--- Assets/ViewDriftRotation.cs
+++ Assets/ViewDriftRotation.cs
@@ -1,33 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ViewDriftRotation : MonoBehaviour
 {
     // 以下为开放配置参数
-    public float rotationAmplitudePitch = 0.50f;    // Pitch 轴最大上下呼吸幅度，用角度表示，此次呼吸 pitch 轴旋转量的极限值
-    public float rotationAmplitudeYaw = 0.50f;      // Yaw 轴最大左右呼吸幅度，用角度表示，此次呼吸 yaw 轴旋转量的极限值
+    public float pbs_ads_stand_pitch_limit = 0.0f;    // Pitch 轴最大上下呼吸幅度，用角度表示，此次呼吸 pitch 轴旋转量的极限值
+    public float pbs_ads_stand_yaw_limit = 0.70f;      // Yaw 轴最大左右呼吸幅度，用角度表示，此次呼吸 yaw 轴旋转量的极限值
     
     // 以下为非开放配置参数
-    public float rotationSpeedPitch = 2.0f;   // Pitch 轴呼吸速度
-    public float rotationSpeedYaw = 2.0f;     // Yaw 轴呼吸速度
-    public float rotationFrequency = 2.5f;    // 呼吸频率，值越大相机完成一次完整旋转周期时间越短，单位时间旋转量越大
+    public float pbs_ads_pitch_speed = 0.70f;   // Pitch 轴呼吸速度
+    public float pbs_ads_yaw_speed = 0.0f;     // Yaw 轴呼吸速度
+    public float pbs_frequency = 2.0f;    // 呼吸频率，值越大相机完成一次完整旋转周期时间越短，单位时间旋转量越大
     
     private Quaternion initialRotation;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
-        Time.timeScale = 0.3f ;
+        Time.timeScale = 1.0f ;
         initialRotation = transform.localRotation;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         // 使用正弦函数来模拟呼吸效果，yaw 轴变化速度默认是 pitch 轴 1/2，安全网设计，不影响最终值
-        float breathPitch = Mathf.Sin(Time.time * rotationSpeedPitch * rotationFrequency) * rotationAmplitudePitch;
-        float breathYaw = Mathf.Sin(Time.time * rotationSpeedYaw * rotationFrequency * 0.5f ) * rotationAmplitudeYaw;
+        float breathPitch = Mathf.Sin(Time.time * pbs_ads_pitch_speed * pbs_frequency) * pbs_ads_stand_pitch_limit;
+        float breathYaw = Mathf.Sin(Time.time * pbs_ads_yaw_speed * pbs_frequency * 0.5f ) * pbs_ads_stand_yaw_limit;
 
         // 创建旋转角度，围绕相机的局部 Y 轴旋转
         Quaternion rotation = Quaternion.Euler(breathPitch, breathYaw, 0);
